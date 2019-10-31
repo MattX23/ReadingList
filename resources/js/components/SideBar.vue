c8eb82889a0950806ccf6ad1f94a1564464681bd
MattX23 <matt.wood001@gmail.com>
2019-10-31 00:13:28 +0000

    Adds sidebar component

---
+++ resources/js/components/SideBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="side-bar">
+<!--        <div class="text-right toggle-container"><span class="toggle-sidebar">X</span></div>-->
+        <div class="menu-item">New List</div>
+        <div class="menu-item">Archives</div>
+        <div class="menu-item">Logout</div>
+
+<!--        <a class="dropdown-item" href="{{ route('logout') }}"-->
+<!--           onclick="event.preventDefault();-->
+<!--                                                     document.getElementById('logout-form').submit();">-->
+<!--            {{ __('Logout') }}-->
+<!--        </a>-->
+
+<!--        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">-->
+<!--            @csrf-->
+<!--        </form>-->
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+
+        },
+        computed: {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style type="scss" scoped>
+    .menu-item {
+        font-size: 1.15rem;
+        text-align: center;
+        border-top: 1px solid silver;
+        padding-top: 0.25rem;
+        line-height: 3.5rem;
+    }
+    .menu-item:hover {
+        background: silver;
+        color: white;
+    }
+    .side-bar {
+        width: 400px;
+        background: white;
+        position: fixed;
+        right: 0;
+        top: 55px;
+    }
+    .toggle-container {
+        height: 20px;
+        margin-bottom: 15px;
+    }
+    .toggle-sidebar {
+        float: right;
+        margin-right: 25px;
+    }
+</style>
