a7ebd88e26e60c33a7ae18b30b0f36d76a3a505c
MattX23 <matt.wood001@gmail.com>
2019-10-31 18:29:36 +0000

    Updates app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,8 +21,6 @@ window.Vue = require('vue');
 
 Vue.component('login-form', require('./components/LoginForm.vue').default);
 Vue.component('reading-list', require('./components/ReadingList.vue').default);
-Vue.component('side-bar', require('./components/SideBar.vue').default);
-Vue.component('toggler', require('./components/NavBarToggler.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
