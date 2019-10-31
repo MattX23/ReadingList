956835c77220e95cd20ea378ec36cc9ae21846b0
MattX23 <matt.wood001@gmail.com>
2019-10-31 00:00:22 +0000

    Adds navbar component for toggling menu

---
+++ resources/js/components/NavBarToggler.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class="">
+        <li @click="toggleSideBar" class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+
+        },
+        computed: {
+
+        },
+        methods: {
+            toggleSideBar() {
+                console.log("Toggle time");
+            },
+        }
+    }
+</script>
+
+<style type="scss" scoped>
+    ul  {
+        list-style: none;
+    }
+</style>
