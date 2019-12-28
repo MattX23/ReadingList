eda04a1bf7da8e23293b564abd7f9f99cd816521
MattX23 <matt.wood001@gmail.com>
2019-12-28 20:44:18 +0000

    Move validation to FormRequests

---
+++ app/Http/Requests/LinkRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LinkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|url',
+            'id'   => 'required|integer',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'You must provide a URL',
+            'name.url'      => 'The URL format is invalid',
+            'id.required'   => 'Something went wrong, please try again',
+        ];
+    }
+}
